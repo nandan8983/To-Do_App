05c7e071cf49d37f41b99c608cf246b608353c15
nandan8983 <103305922+nandan8983@users.noreply.github.com>
2023-07-23 15:38:34 +0530

    1.1

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 app.use(express.json())
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 
