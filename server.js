b87ceea4a2d99cec0b36bc56800908f1f75d22fd
nandan8983 <103305922+nandan8983@users.noreply.github.com>
2023-07-23 15:19:06 +0530

    1.0

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.use(express.json())
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/script.js', (req, res) => {
+    res.sendFile(__dirname + '/views/script.js');
+});
+
+
+
+app.post('/add', (req, res) => {
+    fs.readFile('data.json', (err, data) => {
+        if (err) throw err;
+        if (data == '') data = '[]';
+
+        let dataObj = JSON.parse(data); 
+        dataObj.push(req.body);
+        fs.writeFile('data.json', JSON.stringify(dataObj), (err) => {
+            if (err) throw err;
+            res.send(dataObj);
+        });
+    });
+});
+
+app.get('/todoData', (req, res) => {
+    fs.readFile('data.json', (err, data) => {
+        if (err) throw err;
+        res.send(data);
+    });
+});
+
+
+app.post('/delete', (req, res) => {
+    fs.readFile('data.json', (err, data) => {
+        if (err) throw err;
+        let dataObj = JSON.parse(data);
+        dataObj.forEach((item, index) => {
+            if (item.id == req.body.id) {
+                dataObj.splice(index, 1);
+            }
+        });
+        fs.writeFile('data.json', JSON.stringify(dataObj), (err) => {
+            if (err) throw err;
+            res.send(dataObj);
+        });
+    });
+});
+
+app.post('/update', (req, res) => { 
+    fs.readFile('data.json', (err, data) => {
+        if (err) throw err;
+        let dataObj = JSON.parse(data);
+        dataObj.forEach((item) => {
+            if (item.id == req.body.id) {
+                item.checked = req.body.checked;
+            }
+        });
+        fs.writeFile('data.json', JSON.stringify(dataObj), (err) => {
+            if (err) throw err;
+            res.send(dataObj);
+        });
+    });
+});
+
+
+
+
---
+++ views/script.js
@@ -0,0 +1,102 @@
+
+let inputArea = document.getElementById('inputArea');
+let list = document.getElementById('todoList');
+let listAA = document.getElementById('listAA');
+
+
+inputArea.addEventListener('keypress', (event) => {
+    if (event.key === "Enter" && inputArea.value !== '') {
+        fetch('/add', {
+            method: 'POST',
+            body: JSON.stringify({ item: inputArea.value, checked: false, id: Date.now() }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            res.json().then(async (data) => {
+                await renderList(data);
+            }); });
+        inputArea.value = '';
+    }
+});
+
+fetch('/todoData').then((res) => {
+    res.json().then(async (data) => {
+        await renderList(data);
+    });
+});
+
+
+async function renderList(data){
+    list.innerHTML = '';
+    await data.forEach((item) => {
+        let listItem = document.createElement('li');
+        listItem.className = "listItem";
+        listItem.id = item.id;
+        let checkBox = document.createElement('input');
+        checkBox.type = "checkbox";
+        checkBox.className = "check";
+        if (item.checked === true) {
+            checkBox.checked = true;
+            var InputValue = document.createElement('del');
+        } else if (item.checked === false) {
+            checkBox.checked = false;
+            var InputValue = document.createElement('p');
+        }
+        InputValue.className = "listContent";
+        InputValue.innerHTML = item.item;
+        let deleteButton = document.createElement('button');
+        deleteButton.className = "deleteBtn";
+        deleteButton.innerHTML = "X";
+        listItem.appendChild(InputValue);
+        listItem.appendChild(checkBox);
+        listItem.appendChild(deleteButton);
+        list.appendChild(listItem);
+    });
+    inputArea.style.height = list.offsetHeight+ 143 + "px";
+    
+
+}
+
+
+list.addEventListener('click', (e)=>{
+    if(e.target.tagName === 'INPUT' && e.target.checked === true){
+        updateStatus(e.target.parentElement.firstChild.innerHTML, true, e.target.parentElement.id);
+    }
+    else if(e.target.tagName === 'INPUT' && e.target.checked === false){
+        updateStatus(e.target.parentElement.firstChild.innerHTML, false, e.target.parentElement.id);
+    }
+    else if(e.target.tagName === 'BUTTON'){
+        deleteItem(e.target.parentElement.id);
+
+    }
+}, false)
+
+
+function updateStatus(item, checked, id ) {
+    fetch('/update', {
+        method: 'POST',
+        body: JSON.stringify({ item: item, checked: checked, id: id }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((res) => {
+        res.json().then(async (data) => {
+            await renderList(data);
+        }); }
+    );
+}
+
+function deleteItem(id) {
+    fetch('/delete', {
+        method: 'POST',
+        body: JSON.stringify({ id: id }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((res) => {
+        res.json().then(async (data) => {
+            await renderList(data);
+        }); }
+    );
+}
