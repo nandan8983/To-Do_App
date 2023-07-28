adbf0829ab251d8a6a5f6df5b1228d9fd5f12504
nandan8983 <103305922+nandan8983@users.noreply.github.com>
2023-07-28 23:31:06 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,90 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-app.use(express.json())
+const { v4: uuidv4 } = require('uuid');
+const session = require('express-session');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: 'todoapplication',
+    resave: false,
+    saveUninitialized: true,
+  }))
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
 
 
 app.get('/', (req, res) => {
+    if(!req.session.isLoggedIn) {
+        res.redirect('/login');
+        return
+    } 
+
     res.sendFile(__dirname + '/views/index.html');
+    
 });
+
 app.get('/script.js', (req, res) => {
     res.sendFile(__dirname + '/views/script.js');
 });
 
 
+app.get('/about', (req, res) => {
+    if(!req.session.isLoggedIn) {
+        res.redirect('/login');
+        return
+    } 
+    res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/contact', (req, res) => {
+    if(!req.session.isLoggedIn) {
+        res.redirect('/login');
+        return
+    } 
+    res.sendFile(__dirname + '/views/contact.html');
+});
+
+app.get('/login', (req, res) => {
+    if(req.session.isLoggedIn) {
+        res.redirect('/');
+        return
+    } 
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.post('/login', (req, res) => {
+    
+    let username = req.body.username;
+    let password = req.body.password;
+    if (username == 'admin' && password == 'admin') {
+        req.session.isLoggedIn = true;
+        req.session.username = username; 
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
+});
+
+app.get('/todo', (req, res) => {
+    if(!req.session.isLoggedIn) {
+        res.redirect('/login');
+        return
+    } 
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/home', (req, res) => {
+    res.redirect('/');
+});
 
 app.post('/add', (req, res) => {
     fs.readFile('data.json', (err, data) => {
@@ -73,5 +141,9 @@ app.post('/update', (req, res) => {
 });
 
 
-
+//if url is not found redirect to home page
+app.get('*', (req, res) => {
+    res.redirect('/');
+}
+);
 
